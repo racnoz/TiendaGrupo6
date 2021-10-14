62cd1239fdd44784901c1b604a7f1c30154f2326
Luis Enrique Carmona <Luis Enrique Carmona@DESKTOP-243GD3N>
2021-10-14 13:50:05 -0500

    Back Productos completo

---
+++ src/main/java/com/tienda/DAO/ProductoDAO.java
@@ -0,0 +1,107 @@
+package com.tienda.DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import com.tienda.DTO.ProductoDTO;
+
+public class ProductoDAO {
+	PreparedStatement preparedStatement;
+	public String registrarProducto(ProductoDTO producto) {
+        Conexion conex = new Conexion();
+        try {
+            Statement st = conex.getConnection().createStatement();
+            st.executeUpdate("INSERT INTO productos VALUES ('"+producto.getCodigoProducto()+"', '"
+                     +producto.getIvaCompra()+"', '"+producto.getNitProveedor()+"','"+producto.getNombreProducto()+"','"+producto.getPrecioCompra()+"', '"+producto.getPrecioVenta()+"')");
+            st.close();
+            conex.desconectar();
+            return "Se ha registrado exitosamente el producto";
+            
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+            return "No se pudo agregar el producto";
+        }
+    }
+	public ArrayList<ProductoDTO> consultarProducto(int codigo){
+        ArrayList<ProductoDTO> miProducto = new ArrayList<ProductoDTO>();
+        Conexion conex = new Conexion();
+        try {
+            PreparedStatement consulta = conex.getConnection().prepareStatement("SELECT * FROM productos WHERE codigo_producto = ?");
+            consulta.setInt(1, codigo);
+            ResultSet res = consulta.executeQuery();
+            if(res.next()) {
+                ProductoDTO producto = new ProductoDTO();
+                producto.setCodigoProducto(Integer.parseInt(res.getString("codigo_producto")));
+                producto.setIvaCompra(Float.parseFloat(res.getString("ivacompra")));
+                producto.setNitProveedor(Integer.parseInt(res.getString("nitproveedor")));
+                producto.setNombreProducto(res.getString("nombre_producto"));
+                producto.setPrecioCompra(Float.parseFloat(res.getString("precio_compra")));
+                producto.setPrecioVenta(Float.parseFloat(res.getString("precio_venta")));
+                miProducto.add(producto);
+            }
+            res.close();
+            consulta.close();
+            conex.desconectar();
+        }catch(Exception e) {
+            e.printStackTrace(); 
+        	System.out.print("no se pudo consultar el producto");
+        }
+        return miProducto;
+    }
+	
+	public ArrayList<ProductoDTO> listaDeProductos() {
+        ArrayList< ProductoDTO> miProducto = new ArrayList< ProductoDTO>();
+        Conexion conex= new Conexion();
+        try {
+         PreparedStatement consulta = conex.getConnection().prepareStatement("SELECT * FROM productos");
+         ResultSet res = consulta.executeQuery();
+         while(res.next()){
+        	 ProductoDTO producto = new ProductoDTO();
+             producto.setCodigoProducto(Integer.parseInt(res.getString("codigo_producto")));
+             producto.setIvaCompra(Float.parseFloat(res.getString("ivacompra")));
+             producto.setNitProveedor(Integer.parseInt(res.getString("nitproveedor")));
+             producto.setNombreProducto(res.getString("nombre_producto"));
+             producto.setPrecioCompra(Float.parseFloat(res.getString("precio_compra")));
+             producto.setPrecioVenta(Float.parseFloat(res.getString("precio_venta")));
+             miProducto.add(producto);
+        }
+        res.close();
+        consulta.close();
+        conex.desconectar();
+         
+        } catch (Exception e) {
+         //JOptionPane.showMessageDialog(null, "no se pudo consultar la Persona\n"+e);
+        	e.printStackTrace();
+        }
+        return miProducto;
+    }
+	public String eliminarProducto(int codigo) {
+        Conexion conex= new Conexion();
+        try {
+            String query = "DELETE FROM productos WHERE codigo_producto = ?";
+            preparedStatement = conex.getConnection().prepareStatement(query);
+            preparedStatement.setInt(1, codigo);
+            preparedStatement.executeUpdate();
+            return "Se ha eliminado el producto";
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+            return "No se pudo eliminar el producto";
+        }
+    }
+	
+	public void editarProducto(ProductoDTO producto) {
+		Conexion conex = new Conexion();
+		try {
+			Statement st = conex.getConnection().createStatement();
+			st.executeUpdate("UPDATE productos SET ivacompra = '"+producto.getIvaCompra()+"', nitproveedor = '"+producto.getNitProveedor()+"', nombre_producto='"+producto.getNombreProducto()+"', precio_compra='"+producto.getPrecioCompra()+"', precio_venta='"+producto.getPrecioVenta()+"' WHERE codigo_producto="+producto.getCodigoProducto());
+			st.close();
+			conex.desconectar();
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	} 
+}
---
+++ src/main/java/com/tienda/ProductoController.java
@@ -0,0 +1,51 @@
+package com.tienda;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tienda.DAO.ProductoDAO;
+import com.tienda.DTO.ProductoDTO;
+
+@RestController
+public class ProductoController {
+	
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value="/registrarProducto", method=RequestMethod.POST )
+	public String registrarProducto(ProductoDTO producto) {
+        ProductoDAO Dao = new ProductoDAO(); 
+       return Dao.registrarProducto(producto);
+    }
+	
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value="/consultarProducto", method=RequestMethod.GET)
+	public ArrayList<ProductoDTO> consultarProducto(int codigo){
+        ProductoDAO Dao = new ProductoDAO(); 
+        return Dao.consultarProducto(codigo);
+    }
+	
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value="/listarProductos", method=RequestMethod.GET)
+	public ArrayList<ProductoDTO> listaDeProductos(){
+        ProductoDAO Dao = new ProductoDAO();
+        return Dao.listaDeProductos();
+    }
+	
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value="/eliminarProductos", method=RequestMethod.DELETE)
+    public String eliminarProducto(int codigo) {
+        ProductoDAO Dao = new ProductoDAO();
+        return Dao.eliminarProducto(codigo);
+    }
+		
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value="/editarProducto", method=RequestMethod.POST )
+	public void editarProducto(ProductoDTO producto) {
+		ProductoDAO dao = new ProductoDAO();
+		dao.editarProducto(producto);
+	}
+	
+}

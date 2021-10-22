4eb9c511c7f0c5cbb933941604b1c658834e9a66
Aesirkony <mil.7400@hotmail.com>
2021-10-21 22:38:24 -0500

    eliminando archivos innecesarios

--- tiendaGrupo64/src/main/java/com/mintic/tiendavirtual/DAO/UsuariosDAO.java
+++
@@ -1,9 +0,0 @@
-package com.mintic.tiendavirtual.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mintic.tiendavirtual.modelo.Usuarios;
-
-public interface UsuariosDAO extends JpaRepository<Usuarios, Integer>{
-	
-}
